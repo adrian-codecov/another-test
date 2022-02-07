c2052c188f1a8021e4a8367a1bd1f4e91d96c334
Adrian <adrian@codecov.io>
2022-02-07 15:45:41 -0800

    Added untested code

--- flag1/mafs.js
+++ flag1/mafs.js
@@ -10,5 +10,12 @@ function multiply(a, b) {
     return a * b;
 }
 
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        return 0
+    }
+}
 
 module.exports = {add, subtract, multiply};
