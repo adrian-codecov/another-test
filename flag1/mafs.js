2eacbe5fc7bae4f820c51c3b238ffda3f462e5d5
Adrian <adrian@codecov.io>
2022-02-07 15:43:47 -0800

    commit new test

--- flag1/mafs.js
+++ flag1/mafs.js
@@ -10,12 +10,5 @@ function multiply(a, b) {
     return a * b;
 }
 
-function divide(a, b) {
-    if (b !== 0) {
-        return a / b;
-    } else {
-        return 0
-    }
-}
 
-module.exports = {add, subtract, multiply, divide};
+module.exports = {add, subtract, multiply};
