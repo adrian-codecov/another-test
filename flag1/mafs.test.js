a5ca3a56b7b823c32304512708f9d7cc08b0c598
Rabee-AbuBaker <Rabee@codecov.io>
2022-03-14 14:57:01 +0200

    - Test PR

--- flag1/mafs.test.js
+++ flag1/mafs.test.js
@@ -4,9 +4,9 @@ test('adds 1 + 2 to equal 3', () => {
     expect(add(1, 2)).toBe(3);
 });
 
-// test('subtracts 4 - 1 to equal 3', () => {
-//     expect(subtract(4, 1)).toBe(3);
-// });
+test('subtracts 4 - 1 to equal 3', () => {
+    expect(subtract(4, 1)).toBe(3);
+});
 
 test('multiplies 5 * 6 to equal 30', () => {
     expect(multiply(5, 6)).toBe(30);
