6d9c42f78439743fc9feeb9f8efc627d6d4f10ec
Rabee-AbuBaker <Rabee@codecov.io>
2022-03-14 14:49:59 +0200

    - Another test commit

--- flag1/mafs.test.js
+++ flag1/mafs.test.js
@@ -16,6 +16,6 @@ test('multiplies 5 * 6 to equal 30', () => {
 //     expect(divide(6, 0)).toBe(0);
 // });
 
-test('divides 8 / 2 to equal 3', () => {
-    expect(divide(8, 2)).toBe(4);
-});
+// test('divides 8 / 2 to equal 3', () => {
+//     expect(divide(8, 2)).toBe(4);
+// });
