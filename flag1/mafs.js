64091dd5f1118b2c2acaa7843a54c95d52ac0c9f
Adrian <adrian@codecov.io>
2022-03-23 17:14:54 -0700

    Uncoment everything

--- flag1/mafs.js
+++ flag1/mafs.js
@@ -10,12 +10,12 @@ function multiply(a, b) {
     return a * b;
 }
 
-// function divide(a, b) {
-//     if (b !== 0) {
-//         return a / b;
-//     } else {
-//         return 0
-//     }
-// } 
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        return 0
+    }
+} 
 
 module.exports = {add, subtract, multiply, divide};
