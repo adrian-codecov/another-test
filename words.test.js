d3e252f9e48c0084e635881810cbacb3b94f8e2d
Adrian <adrian@codecov.io>
2022-06-06 17:32:46 -0600

    Add another test at root level

---
+++ words.test.js
@@ -0,0 +1,9 @@
+const {returnPlus3, reverse, hello} = require('./words');
+
+test("returns the given array reversed", function(){
+    expect(reverse([1, 2, 3])).toEqual([3, 2, 1])
+})
+
+test("returns + 3 of whatever you give it", function(){
+    expect(returnPlus3(4)).toBe(7)
+})
