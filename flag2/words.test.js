1496ead87d6e0e073689e05b8c9bf726d96c3722
Adrian <adrian@codecov.io>
2023-03-15 18:22:01 -0700

    more data

--- flag2/words.test.js
+++ flag2/words.test.js
@@ -1,9 +1,16 @@
-const {returnPlus3, reverse, hello} = require('./words');
+const { returnPlus3, reverse, hello } = require("./words");
 
-test("returns the given array reversed", function(){
-    expect(reverse([1, 2, 3])).toEqual([3, 2, 1])
-})
+test("returns the given array reversed", function () {
+  expect(reverse([1, 2, 3])).toEqual([3, 2, 1]);
+});
 
 // test("returns + 3 of whatever you give it", function(){
 //     expect(returnPlus3(4)).toBe(7)
 // })
+
+test("returns hello when 5", function () {
+  expect(hello(5)).toEqual("hello!");
+});
+test("returns nope when not 5", function () {
+  expect(hello(1235)).toEqual("nope");
+});
