2fe03d38fc9eaeed4ac0f3655b224aef5ea31396
Adrian <adrian@codecov.io>
2023-04-18 15:39:32 -0700

    Add codecov.yml for componetns + mafs/words folders

---
+++ mafs/mafs.test.js
@@ -0,0 +1,21 @@
+const {add, subtract, multiply, divide} = require('./mafs');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test('subtracts 4 - 1 to equal 3', () => {
+    expect(subtract(4, 1)).toBe(3);
+}); 
+
+// test('multiplies 5 * 6 to equal 30', () => {
+//     expect(multiply(5, 6)).toBe(30);
+// });  
+
+// test('divides 6 / 0 to equal 0', () => {
+//     expect(divide(6, 0)).toBe(0);
+// });  
+
+// test('divides 8 / 2 to equal 3', () => {
+//     expect(divide(8, 2)).toBe(4);
+// });  
