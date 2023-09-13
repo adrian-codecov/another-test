8eb33d053f78a13946f3cda64159e1e230b87df7
Adrian <adrian@codecov.io>
2023-09-13 12:47:31 -0600

    Add os files and scripts to run them individually

---
+++ os_files/os_files.js
@@ -0,0 +1,12 @@
+function returnWindows() {
+  return "Windows";
+}
+
+function returnIos() {
+  return "Ios";
+}
+function returnLinux() {
+  return "Linux";
+}
+
+module.exports = { returnWindows, returnIos, returnLinux };
