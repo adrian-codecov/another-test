43be49798999585a682012c93b26d78d0a6ef831
Adrian <adrian@codecov.io>
2024-01-19 14:05:19 -0600

    another change

--- mafs/mafs.js
+++ mafs/mafs.js
@@ -6,9 +6,9 @@ function subtract(a, b) {
   return a - b;
 }
 
-function multiply(a, b) {
-  return a * b;
-}
+// function multiply(a, b) {
+//   return a * b;
+// }
 
 function divide(a, b) {
   if (b !== 0) {
