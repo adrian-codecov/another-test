cf126de1b7cf22242bba70fa93452f7670ff5650
Adrian <adrian@codecov.io>
2024-03-20 11:10:10 -0700

    anotha one

--- flag1/mafs.js
+++ flag1/mafs.js
@@ -6,16 +6,16 @@ function subtract(a, b) {
   return a - b;
 }
 
-// function multiply(a, b) {
-//   return a * b;
-// }
+function multiply(a, b) {
+  return a * b;
+}
 
-// function divide(a, b) {
-//   if (b !== 0) {
-//     return a / b;
-//   } else {
-//     return 0;
-//   }
-// }
+function divide(a, b) {
+  if (b !== 0) {
+    return a / b;
+  } else {
+    return 0;
+  }
+}
 
 module.exports = { add, subtract };
