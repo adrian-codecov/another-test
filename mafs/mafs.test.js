0d29feaa3b8a4ac9e597b01ddcb9ae20ed618548
Adrian <adrian@codecov.io>
2024-04-29 16:50:59 -0600

    anotha

--- mafs/mafs.test.js
+++ mafs/mafs.test.js
@@ -1,4 +1,4 @@
-const { add, subtract, multiply, divide } = require("./mafs");
+const { add, subtract, multiply, divide_two } = require("./mafs");
 
 test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
@@ -12,10 +12,10 @@ test("multiplies 5 * 6 to equal 30", () => {
   expect(multiply(5, 6)).toBe(30);
 });
 
-// test("divides 6 / 0 to equal 0", () => {
-//   expect(divide(6, 0)).toBe(0);
-// });
+test("divides_two 6 / 0 to equal 0", () => {
+  expect(divide_two(6, 0)).toBe(0);
+});
 
-// test('divides 8 / 2 to equal 3', () => {
-//     expect(divide(8, 2)).toBe(4);
-// });
+test("divides_two 8 / 2 to equal 3", () => {
+  expect(divide_two(8, 2)).toBe(4);
+});
