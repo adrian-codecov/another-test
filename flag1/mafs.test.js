672225c05e32739dd7768bcf6e5ae61d88d85ff2
Adrian <adrian@codecov.io>
2024-05-24 16:06:44 -0600

    fashfjaa

--- flag1/mafs.test.js
+++ flag1/mafs.test.js
@@ -16,6 +16,6 @@ test("divides 6 / 0 to equal 0", () => {
   expect(divide(6, 0)).toBe(0);
 });
 
-// test("divides 8 / 2 to equal 3", () => {
-//   expect(divide(8, 2)).toBe(4);
-// });
+test("divides 8 / 2 to equal 3", () => {
+  expect(divide(8, 2)).toBe(4);
+});
