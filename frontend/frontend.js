6bd2ea46d3b708a415e9c94fa869d906f0c1b46d
Adrian <adrian@codecov.io>
2024-05-27 14:27:04 -0600

    random addition

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -14,4 +14,8 @@ function lemurr(a) {
   return a === 3 ? "hele" : "ufa";
 }
 
-module.exports = { returnPlus3, reverse, hello };
+function paloma(a) {
+  return a === 2 ? "wow" : "sad";
+}
+
+module.exports = { returnPlus3, reverse, hello, paloma };
