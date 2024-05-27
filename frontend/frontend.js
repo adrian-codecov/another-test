8c81e4dabf7fd4ac1fd086a61f0fe6bb12f32569
Adrian <adrian@codecov.io>
2024-05-27 14:12:07 -0600

    add flag + cff yaml settings

---
+++ frontend/frontend.js
@@ -0,0 +1,17 @@
+function returnPlus3(a) {
+  return a + 3;
+}
+
+function reverse(array) {
+  return array.reverse();
+}
+
+function hello(a) {
+  return a === 5 ? "hello!" : "nope";
+}
+
+function lemur(a) {
+  return a === 3 ? "hele" : "ufa";
+}
+
+module.exports = { returnPlus3, reverse, hello };
