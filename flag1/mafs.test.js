3980d0ecad9dd29345c7c169a365214253ee811a
Adrian <adrian@codecov.io>
2024-05-28 13:51:32 -0600

    ahjf

--- flag1/mafs.test.js
+++ flag1/mafs.test.js
@@ -4,7 +4,7 @@ test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 });
 
-// test('subtracts 4 - 1 to equal 3', () => {
+// test('subtractss 4 - 1 to equal 3', () => {
 //     expect(subtract(4, 1)).toBe(3);
 // });
 
