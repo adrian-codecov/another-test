d82dfb0cf3d14515b4a602de02ddea504ce46f5c
Adrian <adrian@codecov.io>
2024-05-29 16:38:04 -0600

    asdf

--- frontend/frontend.test.js
+++ frontend/frontend.test.js
@@ -1,4 +1,4 @@
-const { returnPlus3, reverse, hello } = require("./frontend");
+const { returnPlus3, reverse } = require("./frontend");
 
 test("returns the given array reversed", function () {
   expect(reverse([1, 2, 3])).toEqual([3, 2, 1]);
@@ -8,9 +8,9 @@ test("returns + 3 of whatever you give it", function () {
   expect(returnPlus3(4)).toBe(7);
 });
 
-test("hello if 5", function () {
-  expect(hello(5)).toBe("hello!");
-});
+// test("hello if 5", function () {
+//   expect(hello(5)).toBe("hello!");
+// });
 
 // test("nope if 6", function () {
 //   expect(hello(6)).toBe("nope");
