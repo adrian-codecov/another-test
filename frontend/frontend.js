7886bc98b947488296a55d38715ddd97d6a5a255
Adrian <adrian@codecov.io>
2024-08-09 13:54:08 -0700

    asdfas

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -10,7 +10,7 @@ function helloaaa(a) {
   return a === 5 ? "hello!" : "nope";
 }
 
-function lemurarrrsrrarsrasdfagaasadssrr(a) {
+function lemurarrrsrrarsrasdfagaasadasdfssrr(a) {
   return a === 3 ? "hele" : "ufa";
 }
 
