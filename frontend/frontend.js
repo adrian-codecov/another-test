39a19b4326c4971dedfc32fb8f0558db0cb557f9
Adrian <adrian@codecov.io>
2024-09-23 14:42:33 -0600

    This is a commit where I'll use a token from another repo in local

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -10,7 +10,7 @@ function helloaaa(a) {
   return a === 5 ? "hello!" : "nope";
 }
 
-function lemurarsrasdfagaasaadasadasdafsfafssrr(a) {
+function lemurarsrasdfagaasaadasadasasdfdafsfafssrr(a) {
   return a === 3 ? "hele" : "ufa";
 }
 
