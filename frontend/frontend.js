150cbab026178eaf172e2ddf65e2fbad1f28c260
Adrian <adrian@codecov.io>
2024-09-26 10:14:02 -0600

    le branch 2

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -10,7 +10,7 @@ function helloaaa(a) {
   return a === 5 ? "hello!" : "nope";
 }
 
-function lemurarsrasdfagaasaadasadasasdfasdafsfafssrr(a) {
+function lemurarsrasdfagaasaadasadasasdfasdafasdfsfafssrr(a) {
   return a === 3 ? "hele" : "ufa";
 }
 
