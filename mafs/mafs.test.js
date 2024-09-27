5d5d0232d7dc52bb0dc9971a6cec007048632fef
Adrian <adrian@codecov.io>
2024-09-26 18:44:24 -0600

    asdf

--- mafs/mafs.test.js
+++ mafs/mafs.test.js
@@ -4,7 +4,7 @@ test("adds 1 + 2 to equal 3", () => {
   expect(add(1, 2)).toBe(3);
 });
 
-// test("subtracts 4 - 1 to equal 3", () => {
+// test("subtracts 4 - 1 to equal 3", () => {a
 //   expect(subtract(4, 1)).toBe(3);
 // });
 
