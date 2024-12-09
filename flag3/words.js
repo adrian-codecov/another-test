cb06946f4827017b959f903c1bc555889ab208fb
Adrian <adrian@codecov.io>
2024-12-09 13:32:18 -0500

    add new covered file

---
+++ flag3/words.js
@@ -0,0 +1,13 @@
+function returnPlus3(a) {
+  return a + 3;
+}
+
+function reverse(array) {
+  return array.reverse();
+}
+
+function hello(a) {
+  return a === 5 ? "hello!" : "nope";
+}
+
+module.exports = { returnPlus3, reverse, hello };
