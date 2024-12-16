984ce176a20365e1c90f3208a405a685d2c183c7
Adrian <adrian@codecov.io>
2024-12-16 12:24:34 -0500

    another time

--- mafs/mafs.test.js
+++ mafs/mafs.test.js
@@ -1,7 +1,7 @@
 const { add, subtract, multiply, divide } = require("./mafs");
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(33);
 });
 
 // test("subtracts 4 - 1 to equal 3", () => {
