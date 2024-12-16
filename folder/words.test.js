cb4162b5ddeabfb395d631fb6f749f0eb9ef791b
Adrian <adrian@codecov.io>
2024-12-16 12:23:15 -0500

    change tests again

--- folder/words.test.js
+++ folder/words.test.js
@@ -5,5 +5,5 @@ test("returns the given array reversed", function () {
 });
 
 test("returns + 3 of whatever you give it", function () {
-  expect(returnPlus3(4)).toBe(7);
+  expect(returnPlus3(4)).toBe(73);
 });
