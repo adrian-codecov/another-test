b6e2bc56a0ba0cd2e03c13a3689f3b0f849a517a
Adrian <adrian@codecov.io>
2024-12-23 13:15:54 -0600

    remove test

--- words/words.test.js
+++ words/words.test.js
@@ -1,6 +1,6 @@
 const {returnPlus3, hello} = require('./words');
 
 
-test("returns + 3 of whatever you give it", function(){
-    expect(returnPlus3(4)).toBe(7)
-})
+// test("returns + 3 of whatever you give it", function(){
+//     expect(returnPlus3(4)).toBe(7)
+// })
