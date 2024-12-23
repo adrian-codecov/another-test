bbb196244551c2cc6dcad7c4973d7b26fc1b26f5
Adrian <adrian@codecov.io>
2024-12-23 13:10:41 -0600

    remove code

--- words/words.js
+++ words/words.js
@@ -2,12 +2,9 @@ function returnPlus3(a) {
     return a + 3
 }
 
-function reverse(array) {
-    return array.reverse();;
-}
 
 function hello(a) {
     return a === 5 ? "hello!" : "nope"
 }
 
-module.exports = {returnPlus3, reverse};
+module.exports = {returnPlus3};
--- words/words.test.js
+++ words/words.test.js
@@ -1,8 +1,5 @@
-const {returnPlus3, reverse, hello} = require('./words');
+const {returnPlus3, hello} = require('./words');
 
-test("returns the given array reversed", function(){
-    expect(reverse([1, 2, 3])).toEqual([3, 2, 1])
-})
 
 test("returns + 3 of whatever you give it", function(){
     expect(returnPlus3(4)).toBe(7)
